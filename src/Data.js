eedd0fdbe466100965a1d8afe41e7e857aeb187e
Sheikhwaseem28 <sheikhwaseem700800@gmail.com>
2024-10-28 11:29:40 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,93 @@
+import project1 from "./assets/projects/project2.jpg";
+import project2 from "./assets/projects/project3.jpg";
+import project3 from "./assets/projects/project1.jpg";
+import project5 from "./assets/projects/Project5.jpg";
+import project6 from "./assets/projects/Project6.jpg";
+import project7 from "./assets/projects/Project7.jpg";
+
+ export const HERO_CONTENT = `I am an enthusiastic and driven Fullstack Developer dedicated to crafting dynamic and responsive web applications. I leverage my expertise in frontend and backend technologies, including HTML, CSS, TailwindCSS, JavaScript, and ReactJS, alongside server-side scripting with NodeJS, Express, and MongoDB. With strong problem-solving and critical thinking skills, I approach challenges with creativity and precision. Passionate about continuous growth, I actively pursue opportunities to enhance my abilities and contribute to innovative projects. I embrace challenges and strive to deliver high-quality solutions that push boundaries and exceed expectations.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Fullstack Developer passionate about creating efficient and user-friendly web applications. With hands-on experience in technologies such as React, Node.js, Express, and MongoDB, I have built a solid foundation in web development. My journey began from a deep curiosity about how things work, evolving into a career where I continuously learn and adapt to new challenges. I thrive in collaborative environments and excel at solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Jan 2024 - Mar 2024",
+    role: "Industrial Training",
+    company: "CodeQuotient Pvt. Lmt.",
+    description: `Mastered the fundamentals of web development, including HTML, CSS, JavaScript, and Node.js, during my industrial training at CodeQuotient.
+Gained hands-on experience by building web applications and crafting interactive user interfaces.
+Implemented server-side technologies, strengthening my foundation in both frontend and backend development.`,
+    technologies: ["HTML","CSS","Javascript", "NodeJS"],
+  },
+  {
+    year: "Aug 2024 - Oct 2024",
+    role: "Frontend Developer Intern",
+    company: "LoveInProperty Pvt. Lmt.",
+    description: `Specialized in creating user-friendly and responsive web interfaces as a Frontend Developer intern at Loveinproperty
+Pvt. Ltd.
+Collaborated with HTML, CSS, JavaScript, Tailwind CSS, and React.js to bring design concepts to life.
+Ensured websites were visually appealing and optimized for smooth functionality across different devices and browsers.`,
+    technologies: ["ReactJS","TailwindCSS"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "SocialBook ",
+    image: project3,
+    description:
+      `I developed a social media platform where users can sign up, log in, and share posts. The platform fosters connections by allowing users to make friends and engage with content. My focus on seamless user experience and secure authentication enhanced my technical skills and understanding of social networking dynamics.`,
+    technologies: ["MongoDB", "ReactJS", "ExpressJS", "NodeJS"],
+    link:"https://github.com/Sheikhwaseem28/social-media",
+  },
+  {
+    title: "NewsWeb",
+    image: project1,
+    description:
+      `I developed a news API web application that aggregates articles from various sources, providing users with real-time access to the latest news. By leveraging multiple APIs, I focused on creating an intuitive layout and seamless navigation, enhancing my skills in API integration and presenting data in a user-friendly manner.`,
+    technologies: ["ReactJS", "TailwindCSS"],
+    link:"https://github.com/Sheikhwaseem28/News_Website",
+  },
+  {
+    title: "MoonFlix",
+    image: project2,
+    description:
+      `I developed an API-based movie website that allows users to sign up, log in, and explore a vast database of films. The platform includes secure authentication and personalized features, enabling users to save favorites and leave reviews. This project enhanced my skills in API integration and user experience design.`,
+    technologies: ["MongoDB", "ReactJS", "Expressjs", "NodeJS"],
+    link:"https://github.com/Sheikhwaseem28/Movie_web",
+  },
+  {
+    title: "CodeVR (Live Now)",
+    image: project5,
+    description:
+      `Step into the future of coding with CodeVR! Experience immersive learning that transforms programming into an exhilarating adventure. Engage with interactive tutorials, tackle hands-on challenges, and collaborate with a vibrant community. Elevate your skills and unleash your potential in a dynamic virtual reality environment. Start your journey today!`,
+    technologies: ["ReactJS", "TailwindCSS"],
+    link:"https://sheikhwaseem28.github.io/CodeVR/",
+  },
+  {
+    title: "FoodLounge (Live Now)",
+    image: project6,
+    description:
+      `Savor the flavors of our exquisite menu, featuring a delightful blend of local and international cuisine. Enjoy a cozy ambiance perfect for gatherings, romantic dinners, or quick bites. With fresh ingredients and a passion for culinary excellence, FoodLounge invites you to indulge in a memorable dining experience. Join us today!`,
+    technologies: ["HTML", "CSS","Javascript"],
+    link:"https://sheikhwaseem28.github.io/FoodLounge/",
+  },
+  {
+    title: "Task Manager (Live Now)",
+    image: project7,
+    description:
+      `Streamline your productivity with our intuitive task management tool. Organize your projects, set deadlines, and prioritize your tasks effortlessly. Collaborate with teams in real-time, track progress, and stay focused on what matters most. With Task Manager, take control of your workflow and achieve your goals with ease. Start optimizing your tasks today!`,
+    technologies: ["Reactjs", "TailwindCSS"],
+    link:"https://sheikhwaseem28.github.io/Task-manager/",
+  },
+
+  
+];
+
+
+
+export const CONTACT = {
+  address: "Moti Nagar Raipur , Chhattishgarh ",
+  phoneNo: "+91 9171417168 ",
+  email: "sheikhwaseem700800@gmail.com",
+};
